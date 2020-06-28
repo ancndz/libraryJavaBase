625bf1891b4f4fdf60889a309d5845f75b0649b3
ancndz <ytkaev12@live.com>
2020-06-28 16:31:40 +0300

    merge fix

--- src/main/java/ru/ancndz/libraryBase/configs/controllers/MainController.java
+++ src/main/java/ru/ancndz/libraryBase/configs/controllers/MainController.java
@@ -2,15 +2,12 @@ package ru.ancndz.libraryBase.configs.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.ancndz.libraryBase.configs.services.LoginService;
-import ru.ancndz.libraryBase.configs.services.UserService;
-import ru.ancndz.libraryBase.content.entity.User;
+import ru.ancndz.libraryBase.content.entity.LibraryUser;
 import ru.ancndz.libraryBase.content.entity.UserExtras;
-import ru.ancndz.libraryBase.content.libraryEnvironment.Staff;
 
 @Controller
 public class MainController {
@@ -25,22 +22,12 @@ public class MainController {
     @GetMapping("")
     public String mainPage(Authentication authentication, Model model) {
         String username = "Stranger";
-        if (authentication != null) {
-            if (authentication.isAuthenticated()) {
-                if (authentication.getPrincipal() instanceof UserDetails) {
-                    username = ((UserDetails)authentication.getPrincipal()).getUsername();
-                } else {
-                    username = authentication.getPrincipal().toString();
-                }
-                UserDetails userDetails = this.loginService.loadUserByUsername(username);
-                if (userDetails instanceof User) {
-                    UserExtras extras = ((User) userDetails).getUserExtras();
-                    username = extras.getFirstName() + " " + extras.getLastName();
-                } else if (userDetails instanceof Staff) {
-                    username = ((Staff) userDetails).getFirstName() + " " + ((Staff) userDetails).getLastName();
-                }
 
-            }
+        LibraryUser libraryUser = this.loginService.loadByAuth(authentication);
+
+        if (libraryUser != null) {
+            UserExtras extras = libraryUser.getUserExtras();
+            username = extras.getFirstName() + " " + extras.getLastName();
         }
         model.addAttribute("name", username);
         //System.out.println(username);
